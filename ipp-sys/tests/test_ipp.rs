e7865ba6d124c1ccc473eb3cae2754058db944e2
Andrew Straw <strawman@astraw.com>
2019-11-23 10:33:53 +0100

    test of whether ippcv is linked is more explicit

--- ipp-sys/tests/test_ipp.rs
+++ ipp-sys/tests/test_ipp.rs
@@ -17,6 +17,8 @@ fn test_link_ippcore() {
     // And compare the minor version, too.
     assert_eq!( linked_version_minor as i32, ipp::IPP_VERSION_MINOR as i32);
 
+    // Despite ippInit not having ippcore in the name, it is defined in
+    // ippcore.h and presumably in the ippcore library.
     ipp_assert!(ipp::ippInit());
     println!("ippcore OK");
 }
@@ -43,20 +45,11 @@ fn test_link_ippi() {
 
 #[test]
 fn test_link_ippcv() {
-    // Not calling ippInit() because allegedly this is not necessary
-    // and this test partly checks if linking is OK without explicitly
-    // pulling in ippcore. ippInit() is defined in ippcore.
-    const W: ::std::os::raw::c_int = 20;
-    const H: ::std::os::raw::c_int = 20;
-    let size = ipp::IppiSize { width: W, height: H };
-
-    let src = [10u8; (W*H) as usize];
-    let mut dest = [0u8; (W*H) as usize];
-
-    assert!(dest[0]==0);
-    ipp_assert!(ipp::ippiAbsDiffC_8u_C1R( src.as_ptr(), W, dest.as_mut_ptr(), W, size, 9));
-    assert!(dest[0]==1);
-    println!("ippcv OK");
+    // Get the version from IPP at runtime.
+    let linked_version_major = unsafe{ (*ipp::ippcvGetLibVersion()).major };
+    let linked_version_minor = unsafe{ (*ipp::ippcvGetLibVersion()).minor };
+    println!("ippcv version {}.{} OK",
+        linked_version_major, linked_version_minor);
 }
 
 #[test]
